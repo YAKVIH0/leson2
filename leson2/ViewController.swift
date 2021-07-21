eb1ed4716ebb1b51c0547d38fdd047d97d8f67bb
yakvih <yakvih@icloud.com>
2021-07-21 21:50:54 +0300

    code from leson2

--- leson2/ViewController.swift
+++ leson2/ViewController.swift
@@ -11,9 +11,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let age = 23
+        
+        if age < 10 {
+             print ("Ребенок")
+        } else  if age >= 10 && age < 18 {
+            print ("Подросток")
+       } else  if age >= 18 && age < 30 {
+        print ("Молодой")
+   } else  if age >= 30 && age < 60 {
+    print ("Взлослый")
+   } else  if age >= 60 {
+    print ("Пожилой")
+    
+    
+ 
+    
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+}
